e93d9fe939384fb051cacd6be2070453aa7e5a2b
hao <unnamed.hao@gmail.com>
2022-03-08 12:18:45 +0800

    init

--- android/src/main/java/com/reactnativetrackingiomodule/TrackingioModuleModule.java
+++ android/src/main/java/com/reactnativetrackingiomodule/TrackingioModuleModule.java
@@ -1,12 +1,17 @@
 package com.reactnativetrackingiomodule;
 
+import android.content.res.Resources;
+
 import androidx.annotation.NonNull;
 
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.reyun.tracking.sdk.InitParameters;
+import com.reyun.tracking.sdk.Tracking;
+import com.facebook.react.bridge.Callback;
 
 @ReactModule(name = TrackingioModuleModule.NAME)
 public class TrackingioModuleModule extends ReactContextBaseJavaModule {
@@ -22,13 +27,80 @@ public class TrackingioModuleModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void addListener(String eventName) {
+
     }
 
-    public static native int nativeMultiply(int a, int b);
+    @ReactMethod
+    public void removeListeners(Integer count) {
+
+    }
+
+    @ReactMethod
+    public void initWithKeyAndChannelId(String appkey, String channelId) {
+      ReactApplicationContext reactContext = this.getReactApplicationContext();
+
+      // 创建初始化参数对象
+      InitParameters parameters = new InitParameters();
+      // 初始化参数appkey，必须
+      parameters.appKey = appkey;
+      // 初始化参数channelId, 必须
+      int reyunChannelID = reactContext.getResources().getIdentifier("REYUN_CHANNEL", "string", reactContext.getPackageName());
+      try{
+        String reyunChannel = reactContext.getResources().getString(reyunChannelID);
+        parameters.channelId = reyunChannel;
+      } catch (Resources.NotFoundException e) {
+        parameters.channelId = "_default_";
+      }
+      // 初始化参数oaid，如果传入oaid，那么热云sdk不再调用msa相关接口获取oaid
+      // parameters.oaid = "hahahahahahaha,oaid";
+      // 初始化参数assetFileName, 使用oaid版本1.0.26+以上时，需要此参数
+      // （该参数需向msa申请证书进行获取）
+      // parameters.assetFileName = "com.reyun.chicken.cert.pem";
+      // 初始化参数oaidLibraryString, 使用oaid版本1.0.26+以上时，需要此参数
+      // 根据msa文档1.0.26对应值为 nllvm1623827671，1.0.27对应值为 nllvm1630571663641560568，1.0.30对应值为msaoaidsec
+      //  parameters.oaidLibraryString = "msaoaidsec";
+      Tracking.initWithKeyAndChannelId(reactContext.getCurrentActivity().getApplication(), parameters);
+    }
+
+    @ReactMethod
+    public void setRegisterWithAccountID(String accountId) {
+      Tracking.setRegisterWithAccountID(accountId);
+    }
+
+    @ReactMethod
+    public void setLoginSuccessBusiness(String accountId) {
+      Tracking.setLoginSuccessBusiness(accountId);
+    }
+
+    @ReactMethod
+    public void setEvent(String eventName, ReadableMap m) {
+      Tracking.setEvent(eventName,m.toHashMap());
+    }
+
+    @ReactMethod
+    public void setAdShow(String adPlatform, String adId, String fill) {
+      Tracking.setAdShow(adPlatform, adId, fill);
+    }
+
+    @ReactMethod
+    public void setAdClick(String adPlatform, String adId) {
+      Tracking.setAdClick(adPlatform, adId);
+    }
+
+    @ReactMethod
+    public void setAppDuration(int duration) {
+      Tracking.setAppDuration(duration);
+    }
+
+    @ReactMethod
+    public void setPageDuration(String activityName, int duration) {
+      Tracking.setPageDuration(activityName, duration);
+    }
+
+    @ReactMethod
+    public void getDeviceId(Callback successCallback) {
+      successCallback.invoke(Tracking.getDeviceId());
+    }
 }
--- ios/TrackingioModule.m
+++ ios/TrackingioModule.m
@@ -1,19 +1,59 @@
 #import "TrackingioModule.h"
+#import "Tracking.h"
 
 @implementation TrackingioModule
 
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(initWithKeyAndChannelId:(NSString *)appKey location:(NSString *)channelId)
 {
-  NSNumber *result = @([a floatValue] * [b floatValue]);
+  [Tracking initWithAppKey:appKey withChannelId:@"_default_"];
+}
 
-  resolve(result);
+RCT_EXPORT_METHOD(setRegisterWithAccountID:(NSString *)account)
+{
+  [Tracking setRegisterWithAccountID:account];
+}
+
+RCT_EXPORT_METHOD(setLoginSuccessBusiness:(NSString *)account)
+{
+  [Tracking setLoginWithAccountID:account];
+}
+
+RCT_EXPORT_METHOD(setEvent:(NSString *)eventName param:(nullable NSDictionary *)custom_params)
+{
+  [Tracking setEvent:eventName param:custom_params];
+}
+
+RCT_EXPORT_METHOD(setAdShow:(NSString *)adPlatform adId:(NSString *)adId isSuccess:(NSString *)playSuccess)
+{
+  int ok = 0;
+  if ([playSuccess isEqualToString:@"1"]){
+    ok = 1;
+  }
+  [Tracking onAdShow:adPlatform adId:adId isSuccess:ok];
+}
+
+RCT_EXPORT_METHOD(setAdClick:(NSString *)adPlatform adId:(NSString *)adId)
+{
+  [Tracking onAdClick:adPlatform adId:adId];
+}
+
+RCT_EXPORT_METHOD(setAppDuration:(int)duration)
+{
+  [Tracking setTrackAppDuration:duration];
+}
+
+RCT_EXPORT_METHOD(setPageDuration:(NSString *)pageID duration:(int)duration)
+{
+  [Tracking trackViewName:pageID duration:duration];
+}
+
+
+RCT_EXPORT_METHOD(getDeviceId:(RCTResponseSenderBlock)callback)
+{
+  NSString *did = [Tracking getDeviceId];
+  callback(@[did]);
 }
 
 @end
